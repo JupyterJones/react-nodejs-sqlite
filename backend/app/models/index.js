70b439604a5ef6071f5cb1c332ad505b487fa566
JupyterJones <jack.northrup.ph@gmail.com>
2022-02-18 09:01:55 +0800

    everything update Using node -v 14

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -12,16 +12,24 @@ const sequelize = new Sequelize(config);
 
 fs
   .readdirSync(__dirname)
-  .filter(file => {
+  .filter(file =>
+  {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+  .forEach(file =>
+  {
+    // Original:
+    //pre update const model = sequelize['import'](path.join(__dirname, file));
+    //changed to:
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    // Change per https://stackoverflow.com/questions/62917111/sequelize-import-is-not-a-function
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
+Object.keys(db).forEach(modelName =>
+{
+  if (db[modelName].associate)
+  {
     db[modelName].associate(db);
   }
 });
